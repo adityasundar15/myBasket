32e0a9c189ac0c5085f1b6bbb593e823eb2e8b6a
gunjan <sgunjan091@gmail.com>
2023-04-01 22:14:04 +0900

    stock_screen & cocoapods (1.11->1.12) update

--- lib/screens/stock_screen.dart
+++ lib/screens/stock_screen.dart
@@ -70,85 +70,88 @@ class _StockScreenState extends State<StockScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 15),
-          child: ElevatedButton(
-            style: ButtonStyle(
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25.0),
+      body: Expanded(
+        child: Column(children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 15),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
                 ),
               ),
-            ),
-            onPressed: () {
-              Navigator.of(context).pushNamed(AlertList.routeName);
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  // decoration: BoxDecoration(
-                  //     borderRadius: BorderRadius.all(Radius.circular(10)),
-                  //     border: Border.all(color: Colors.purple)),
-                  child: Icon(
-                    size: 28,
-                    Icons.warning_amber,
-                    color: ColorOptions.colorscheme[50],
+              onPressed: () {
+                Navigator.of(context).pushNamed(AlertList.routeName);
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(10),
+                    // decoration: BoxDecoration(
+                    //     borderRadius: BorderRadius.all(Radius.circular(10)),
+                    //     border: Border.all(color: Colors.purple)),
+                    child: Icon(
+                      size: 28,
+                      Icons.warning_amber,
+                      color: ColorOptions.colorscheme[50],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Text(
-                    'Expiry alerts',
-                    style: TextStyle(
-                        fontSize: 24, color: ColorOptions.colorscheme[50]),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      'Expiry alerts',
+                      style: TextStyle(
+                          fontSize: 24, color: ColorOptions.colorscheme[50]),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-        TableCalendar<Item>(
-          focusedDay: _focusedDate,
-          firstDay: DateTime.utc(2023, 1, 1),
-          lastDay: DateTime.utc(2024, 12, 31),
-          selectedDayPredicate: (day) {
-            return isSameDay(_selectedDate, day);
-          },
-          onDaySelected: _onDaySelected,
-          calendarFormat: _calendarFormat,
-          onFormatChanged: (format) {
-            if (_calendarFormat != format) {
-              setState(() {
-                _calendarFormat = format;
-              });
-            }
-          },
-          onPageChanged: (focusedDate) {
-            _focusedDate = focusedDate;
-          },
-          eventLoader: _getItemsForDay,
-          calendarStyle: CalendarStyle(
-            selectedDecoration: BoxDecoration(
-              color: ColorOptions.colorscheme[500]!,
-              shape: BoxShape.circle,
-            ),
-            todayDecoration: BoxDecoration(
-                color: ColorOptions.colorscheme[300]!, shape: BoxShape.circle),
-          ),
-        ),
-        const SizedBox(height: 8.0),
-        Expanded(
-          child: ValueListenableBuilder<List<Item>>(
-            valueListenable: _selectedItems,
-            builder: (context, value, _) {
-              return StockItemList(itemList: value);
+          TableCalendar<Item>(
+            focusedDay: _focusedDate,
+            firstDay: DateTime.utc(2023, 1, 1),
+            lastDay: DateTime.utc(2024, 12, 31),
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDate, day);
             },
+            onDaySelected: _onDaySelected,
+            calendarFormat: _calendarFormat,
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              }
+            },
+            onPageChanged: (focusedDate) {
+              _focusedDate = focusedDate;
+            },
+            eventLoader: _getItemsForDay,
+            calendarStyle: CalendarStyle(
+              selectedDecoration: BoxDecoration(
+                color: ColorOptions.colorscheme[500]!,
+                shape: BoxShape.circle,
+              ),
+              todayDecoration: BoxDecoration(
+                  color: ColorOptions.colorscheme[300]!,
+                  shape: BoxShape.circle),
+            ),
           ),
-        )
-      ]),
+          const SizedBox(height: 8.0),
+          Expanded(
+            child: ValueListenableBuilder<List<Item>>(
+              valueListenable: _selectedItems,
+              builder: (context, value, _) {
+                return StockItemList(itemList: value);
+              },
+            ),
+          )
+        ]),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showAddItem(context);
